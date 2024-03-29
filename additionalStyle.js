54b7a104708545bdcd198a9bd73d77dd94612882
Ritik Gupta <ritik.mpm@gmail.com>
2024-03-29 22:47:44 +0530

    Add files via upload

---
+++ additionalStyle.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+    extend: {
+        colors: {
+            primaryGreen: 'rgb(101 197 28)',
+            secondaryGreen: 'rgb(21 41 10)',
+            secondaryGray: 'rgb(245 245 244)'
+        }
+    }
+    }
+}
